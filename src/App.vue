8b40c612c7ba4cfee791c994677180728c103ff6
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-12-19 20:27:31 -0300

    config inicial do projeto

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import HeroSection from './components/HeroSection.vue'
+import Portfolio from './components/Portfolio.vue'
+import AboutUs from './components/AboutUs.vue'
+import ContactForm from './components/ContactForm.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <Header />
+    <main>
+      <HeroSection />
+      <Portfolio />
+      <AboutUs />
+      <ContactForm />
+    </main>
+    <Footer />
+  </div>
+</template>
